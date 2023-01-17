69fbb69c2f1950c5dffb44937c9d16471f6aee50
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2023-01-17 17:40:39 -0500

    Update stream.py

--- stream.py
+++ stream.py
@@ -29,6 +29,7 @@ import json
 import socket 
 import logging
 from jsonpath_ng import jsonpath, parse
+import re
 
 #### https://github.com/tspannhw/FLiP-PulsarDevPython101/
 
@@ -77,37 +78,39 @@ class mastodondata(Record):
     statuses_count = Integer()
 
 
+keywordList = ['apache spark','Apache Spark', 'Apache Pinot','flink','Flink','Apache Flink','kafka', 'Kafka', 'Apache Kafka', 'pulsar', 'Pulsar', 'datapipeline', 'real-time', 'real-time streaming', 'StreamNative', 'Confluent', 'RedPandaData', 'Apache Pulsar', 'streaming', 'Streaming', 'big data', 'Big Data']
+
+words_re = re.compile("|".join(keywordList))
 
 class Listener(mastodon.StreamListener):
 
     def on_update(self, status):
-        #print(f"on_update: {status}")
-        #producer.send('rp4-kafka-1', json.dumps(row).encode('utf-8'))
-        #producer.flush()
-        pulsarProducer = pulsarClient.create_producer(topic='persistent://public/default/mastodon',
-            schema=JsonSchema(mastodondata), properties={"producer-name": "mastodon-py-strean","producer-id": "mastodon-producer" })
-        mastodonRec = mastodondata()
-        uuid_key = '{0}_{1}'.format(strftime("%Y%m%d%H%M%S",gmtime()),uuid.uuid4())
-        mastodonRec.language = status.language
-        mastodonRec.created_at = str(status.created_at)
-        mastodonRec.ts = float(strftime("%Y%m%d%H%M%S",gmtime()))
-        mastodonRec.uuid = uuid_key
-        mastodonRec.uri = status.uri
-        mastodonRec.url = status.url
-        mastodonRec.favourites_count = status.favourites_count
-        mastodonRec.replies_count = status.replies_count
-        mastodonRec.reblogs_count = status.reblogs_count
-        mastodonRec.content = status.content 
-        mastodonRec.username = status.account.username
-        mastodonRec.accountname = status.account.acct
-        mastodonRec.displayname = status.account.display_name
-        mastodonRec.note = status.account.note
-        mastodonRec.followers_count = status.account.followers_count
-        mastodonRec.statuses_count = status.account.statuses_count
-        print(mastodonRec.ts)
-        pulsarProducer.send(mastodonRec,partition_key=str(uuid_key))
-        pulsarProducer.flush()
-
+        if words_re.search(status.content):
+            pulsarProducer = pulsarClient.create_producer(topic='persistent://public/default/mastodon',
+                schema=JsonSchema(mastodondata), properties={"producer-name": "mastodon-py-strean","producer-id": "mastodon-producer" })
+            mastodonRec = mastodondata()
+            uuid_key = '{0}_{1}'.format(strftime("%Y%m%d%H%M%S",gmtime()),uuid.uuid4())
+            mastodonRec.language = status.language
+            mastodonRec.created_at = str(status.created_at)
+            mastodonRec.ts = float(strftime("%Y%m%d%H%M%S",gmtime()))
+            mastodonRec.uuid = uuid_key
+            mastodonRec.uri = status.uri
+            mastodonRec.url = status.url
+            mastodonRec.favourites_count = status.favourites_count
+            mastodonRec.replies_count = status.replies_count
+            mastodonRec.reblogs_count = status.reblogs_count
+            mastodonRec.content = status.content 
+            mastodonRec.username = status.account.username
+            mastodonRec.accountname = status.account.acct
+            mastodonRec.displayname = status.account.display_name
+            mastodonRec.note = status.account.note
+            mastodonRec.followers_count = status.account.followers_count
+            mastodonRec.statuses_count = status.account.statuses_count
+            print(mastodonRec.ts)
+            pulsarProducer.send(mastodonRec,partition_key=str(uuid_key))
+            pulsarProducer.flush()
+            #producer.send('rp4-kafka-1', mastodonRec.encode('utf-8'))
+            #producer.flush()
 
     def on_notification(self, notification):
         # print(f"on_notification: {notification}")
